544bd2cdeeaa611e32d8b649c5ecb1e4dd61dd7e
geekplux <geekplux@qq.com>
2017-06-22 02:47:42 +0800

    [tests] added test for promisify

--- tests/utils.spec.js
+++ tests/utils.spec.js
@@ -1,7 +1,20 @@
-import { colour } from '../src/utils.js';
+import { colour, promisify } from '../src/utils.js';
 
 test('color generator', () => {
   expect(colour).toBeDefined();
   expect(colour(0)).toBe(2062260);
   expect(colour(1)).toBe(11454440);
+});
+
+test('promisify', () => {
+  function isPromise (obj) {
+    return !!obj &&
+      (typeof obj === 'object' || typeof obj === 'function') &&
+      typeof obj.then === 'function';
+  }
+  const d = {
+    f: () => {}
+  };
+  expect(promisify).toBeDefined();
+  expect(promisify(d, d.f)).toBeTruthy();
 });
