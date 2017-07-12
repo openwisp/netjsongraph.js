e39a49a5d6ffb515dc0db8554d40174a413389d6
geekplux <geekplux@qq.com>
2017-07-12 09:24:41 +0800

    [tests] improve tests for constructor and load function

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,4 +1,5 @@
 import Netjsongraph from '../src/netjsongraph.three.js';
+import netjsonData from '../examples/data/netjson.json';
 
 test('Module importing', () => {
   expect(Netjsongraph).toBeDefined();
@@ -7,17 +8,39 @@ test('Module importing', () => {
 
 describe('API', () => {
   test('constructor', () => {
-    const ng = new Netjsongraph();
+    const testUrl = 'testUrl';
+    const ng = new Netjsongraph(testUrl);
     expect(ng).toHaveProperty('width', window.innerWidth);
     expect(ng).toHaveProperty('height', window.innerHeight);
+    expect(ng).toHaveProperty('url', testUrl);
     expect(ng).toHaveProperty('el', document.body);
-    expect(ng).toHaveProperty('data');
+    expect(ng).toHaveProperty('data', {});
+    expect(ng).toHaveProperty('metadata', true);
+    expect(ng).toHaveProperty('defaultStyle', true);
+    expect(ng).toHaveProperty('scaleExtent', [0.25, 5]);
+    expect(ng).toHaveProperty('linkDistance', 50);
+    expect(ng).toHaveProperty('linkStrength', 0.2);
+    expect(ng).toHaveProperty('theta', 0.8);
+    expect(ng).toHaveProperty('distanceMax', 100);
+    expect(ng).toHaveProperty('circleRadius', 8);
+    expect(ng).toHaveProperty('onInit', null);
+    expect(ng).toHaveProperty('onLoad', null);
+    expect(ng).toHaveProperty('onEnd', null);
+    expect(ng).toHaveProperty('onClickNode', null);
+    expect(ng).toHaveProperty('onClickLink', null);
+    expect(ng).toHaveProperty('initialAnimation', false);
+    expect(ng).toHaveProperty('static', true);
+    expect(ng).toHaveProperty('scene');
+    expect(ng).toHaveProperty('camera');
   });
 
   test('set', () => {
     const ng = new Netjsongraph();
     ng.set({ width: 1000 });
     expect(ng).toHaveProperty('width', 1000);
+
+    ng.set({ height: 500 });
+    expect(ng).toHaveProperty('height', 500);
   });
 
   test('set container element', () => {
@@ -32,12 +55,15 @@ describe('API', () => {
   });
 
   test('load data', () => {
-    const d = {
-      nodes: [],
-      links: []
-    };
     const ng = new Netjsongraph();
-    ng.load(d);
-    expect(ng).toHaveProperty('data', d);
+    ng.load(netjsonData);
+    expect(ng).toHaveProperty('data', netjsonData);
+    expect(ng.data).toHaveProperty('type', netjsonData.type);
+    expect(ng.data).toHaveProperty('label', netjsonData.label);
+    expect(ng.data).toHaveProperty('protocol', netjsonData.protocol);
+    expect(ng.data).toHaveProperty('version', netjsonData.version);
+    expect(ng.data).toHaveProperty('metric', netjsonData.metric);
+    expect(ng.data.nodes).toHaveLength(netjsonData.nodes.length);
+    expect(ng.data.links).toHaveLength(netjsonData.links.length);
   });
 });
