2be013c50e8405251051d1eb98e9689f678bf8de
geekplux <geekplux@qq.com>
2017-07-24 19:00:26 +0800

    [examples] modify examples folder

---
+++ examples/refactor/basic/index.js
@@ -0,0 +1,4 @@
+import Netjsongraph from '../../../src/netjsongraph.three.js';
+
+const ng = new Netjsongraph('../data/netjson.json');
+console.log(ng);
--- examples/refactor/index.js
+++
@@ -1,4 +0,0 @@
-import Netjsongraph from '../../src/netjsongraph.three.js';
-
-const ng = new Netjsongraph('data/netjson.json');
-console.log(ng);
