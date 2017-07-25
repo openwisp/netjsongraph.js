96b9b6b5e750759329ea7198038b71864872075b
geekplux <geekplux@qq.com>
2017-07-25 10:49:44 +0800

    [examples] add callbacks example

---
+++ examples/refactor/callbacks/index.js
@@ -0,0 +1,10 @@
+import Netjsongraph from '../../../src/netjsongraph.three.js';
+
+const ng = new Netjsongraph('../data/netjson.json', {
+  static: false,
+  initialAnimation: true,
+  onInit: () => console.log('initialized'),
+  onLoad: () => console.log('data loaded'),
+  onEnd: () => console.log('animation completed')
+});
+console.log(ng);
---
+++ examples/refactor/network-collection/index.js
@@ -0,0 +1,4 @@
+import Netjsongraph from '../../../src/netjsongraph.three.js';
+
+const ng = new Netjsongraph('../data/netjson-network-collection.json');
+console.log(ng);
