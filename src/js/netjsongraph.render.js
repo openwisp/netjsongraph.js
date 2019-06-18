f3a3057b7fad3cb621d1500a3b21ea37a69ab88f
KuTuGu <256317135@qq.com>
2019-06-18 09:31:59 +0800

    fix: use echarts-leaflet for animation path

--- src/js/netjsongraph.render.js
+++ src/js/netjsongraph.render.js
@@ -2,21 +2,75 @@
 
 import * as echarts from "echarts/lib/echarts";
 import "echarts/lib/chart/graph";
+import "echarts/lib/chart/effectScatter";
+import "echarts/lib/chart/lines";
 import "echarts/lib/component/tooltip";
 import "echarts/lib/component/title";
 import "echarts/lib/component/toolbox";
 import "echarts/lib/component/legend";
 import "zrender/lib/svg/svg";
 
-import "../../lib/js/leaflet.min.js";
-import "../../lib/js/leaflet-mapDownload.js";
-import "../../lib/js/leaflet-draw.js";
+import "leaflet/dist/leaflet.js";
 
-const RenderCache = {
-  netjsonmap: null,
-  viewIndoormap: false,
-  leafLeyLayers: null
-};
+import "echarts-leaflet/dist/echarts-leaflet.js";
+
+/**
+ * @function
+ * @name graphSetOption
+ *
+ * set option in echarts and render.
+ *
+ * @param  {object}  customOption    custom option determined by different render.
+ * @param  {object}  _this           NetJSONGraph object
+ *
+ * @return {object}  graph object
+ *
+ */
+function graphSetOption(customOption, echartsLayer, _this) {
+  const configs = _this.config,
+    commonOption = _this.utils.deepMergeObj(
+      {
+        tooltip: {
+          confine: true,
+          formatter: params => {
+            if (params.componentSubType === "graph") {
+              return params.dataType === "edge"
+                ? _this.utils.linkInfo(params.data)
+                : _this.utils.nodeInfo(params.data);
+            } else {
+              return params.componentSubType === "lines"
+                ? _this.utils.linkInfo(params.data.link)
+                : _this.utils.nodeInfo(params.data.node);
+            }
+          }
+        }
+      },
+      configs.echartsOption
+    );
+
+  echartsLayer.setOption(_this.utils.deepMergeObj(commonOption, customOption));
+  echartsLayer.on(
+    "click",
+    function(params) {
+      let clickElement = configs.onClickElement.bind(_this);
+
+      if (params.componentSubType === "graph") {
+        clickElement(params.dataType === "edge" ? "link" : "node", params.data);
+      } else {
+        return params.componentSubType === "lines"
+          ? clickElement("link", params.data.link)
+          : clickElement("node", params.data.node);
+      }
+    },
+    { passive: true }
+  );
+
+  window.onresize = () => {
+    echartsLayer.resize();
+  };
+
+  return echartsLayer;
+}
 
 /**
  * @function
@@ -27,7 +81,6 @@ const RenderCache = {
  * @param  {object}  JSONData        Render dependent configuration
  * @param  {object}  _this           NetJSONGraph object
  *
- * @return {object}  graph object
  */
 function graphRender(graphContainer, JSONData, _this) {
   let categories = JSONData.categories || [],
@@ -48,7 +101,7 @@ function graphRender(graphContainer, JSONData, _this) {
       if (node.category) {
         nodeResult.category = String(node.category);
       }
-      if (categories.indexOf(node.category) === -1) {
+      if (node.category && categories.indexOf(node.category) === -1) {
         categories.push(node.category);
       }
 
@@ -64,74 +117,33 @@ function graphRender(graphContainer, JSONData, _this) {
 
       return linkResult;
     }),
-    options = {
-      title: configs.title,
-      aria: {
-        show: true,
-        description:
-          "This is a force-oriented graph chart that depicts the relationship between ip nodes."
-      },
-      toolbox: {
-        show: true,
-        feature: {
-          restore: {
-            show: true
-          },
-          saveAsImage: {
-            show: true
-          }
-        }
-      },
-      tooltip: {
-        confine: true,
-        formatter: (params, ticket, callback) =>
-          params.dataType === "edge"
-            ? _this.utils.linkInfo(params.data)
-            : _this.utils.nodeInfo(params.data)
-      },
-      legend: {
-        data: categories
-      },
-      series: [
-        Object.assign(configs.graphConfig, {
-          type: "graph",
-          label: Object.assign(configs.graphConfig.label || {}, {
-            offset: [configs.labelDx, configs.labelDy]
-          }),
-          force: Object.assign(configs.graphConfig.force || {}, {
-            repulsion: configs.repulsion,
-            gravity: configs.gravity,
-            edgeLength: configs.edgeLength
-          }),
-          nodes,
-          links,
-          categories: categories.map(category => ({ name: category }))
-        })
-      ]
-    },
+    series = [
+      Object.assign(configs.graphConfig, {
+        type: "graph",
+        nodes,
+        links,
+        categories: categories.map(category => ({ name: category }))
+      })
+    ],
     graph = echarts.init(graphContainer, null, {
       renderer: configs.svgRender ? "svg" : "canvas"
-    });
-
-  graph.setOption(options);
-  graph.on(
-    "mouseup",
-    function(params) {
-      if (params.componentType === "series" && params.seriesType === "graph") {
-        if (params.dataType === "edge") {
-          configs.onClickLink.call(_this, params.data);
-        } else {
-          configs.onClickNode.call(_this, params.data);
+    }),
+    legend = categories.length
+      ? {
+          data: categories
         }
-      }
-    },
-    { passive: true }
-  );
-  window.onresize = () => {
-    graph.resize();
-  };
+      : undefined;
 
-  return graph;
+  _this.echarts = graphSetOption(
+    {
+      legend,
+      series
+    },
+    graph,
+    _this
+  );
+
+  configs.onLoad.call(_this);
 }
 
 /**
@@ -143,180 +155,104 @@ function graphRender(graphContainer, JSONData, _this) {
  * @param  {object}  JSONData       Render dependent configuration
  * @param  {object}  _this          NetJSONGraph object
  *
- * @return {object}  map object
  */
 function mapRender(mapContainer, JSONData, _this) {
-  if (!RenderCache.netjsonmap) {
-    RenderCache.netjsonmap = L.map(mapContainer, {
-      renderer: _this.config.svgRender ? L.svg() : L.canvas()
-    }).setView([42.168, 260.536], 8);
-  } else {
-    RenderCache.netjsonmap = L.map(mapContainer, {
-      renderer: _this.config.svgRender ? L.svg() : L.canvas()
-    }).setView(
-      RenderCache.netjsonmap.getCenter(),
-      RenderCache.netjsonmap.getZoom()
-    );
-  }
+  let configs = _this.config,
+    { nodes, links } = JSONData,
+    flatNodes = JSONData.flatNodes || {},
+    linesData = [],
+    nodesData = [];
 
-  let map = RenderCache.netjsonmap,
-    editableLayers = new L.FeatureGroup(),
-    MyCustomMarker = L.Icon.extend({
-      options: {
-        shadowUrl: null,
-        iconAnchor: new L.Point(12, 12),
-        iconUrl: "../lib/images/marker-icon.png"
-      }
-    }),
-    options = {
-      position: "topleft",
-      draw: {
-        polyline: {
-          shapeOptions: {
-            color: "#f357a1",
-            weight: 3
-          }
-        },
-        polygon: {
-          allowIntersection: false, // Restricts shapes to simple polygons
-          drawError: {
-            color: "#e1e100", // Color the shape will turn when intersects
-            message: "<strong>Oh snap!<strong> you can't draw that!" // Message that will show when intersect
-          },
-          shapeOptions: {
-            color: "#bada55"
-          }
-        },
-        circle: true, // Turns off this drawing tool
-        rectangle: {
-          shapeOptions: {
-            clickable: false
-          }
-        },
-        marker: {
-          icon: new MyCustomMarker()
-        }
-      },
-      edit: {
-        featureGroup: editableLayers, //REQUIRED!!
-        remove: true
-      }
-    };
-
-  map.addLayer(editableLayers);
-  map.addControl(new L.Control.Draw(options));
-  map.on(L.Draw.Event.CREATED, function(e) {
-    var type = e.layerType,
-      layer = e.layer;
-
-    if (type === "marker") {
-      layer.bindPopup("A popup!");
-    }
-
-    editableLayers.addLayer(layer);
-  });
-
-  L.easyPrint({
-    title: "Awesome print button",
-    position: "bottomleft",
-    exportOnly: true,
-    sizeModes: ["Current", "A4Portrait", "A4Landscape"]
-  }).addTo(map);
-
-  if (!RenderCache.viewIndoormap) {
-    const nodeElements = [],
-      linkElements = [],
-      drawElements = [];
-    let { nodes, links } = JSONData,
-      flatNodes = {};
-    if (JSONData.flatNodes) {
-      flatNodes = JSONData.flatNodes;
+  nodes.map(node => {
+    if (!node.location || !node.location.lng || !node.location.lat) {
+      console.error(`Node ${node.id} position is undefined!`);
     } else {
-      nodes.map(node => {
+      nodesData.push({
+        name: node.name,
+        value: [node.location.lng, node.location.lat],
+        symbolSize:
+          typeof configs.nodeSize === "function"
+            ? configs.nodeSize(node)
+            : configs.nodeSize,
+        itemStyle:
+          typeof configs.nodeStyleProperty === "function"
+            ? configs.nodeStyleProperty(node)
+            : configs.nodeStyleProperty,
+        node
+      });
+      if (!JSONData.flatNodes) {
         flatNodes[node.id] = JSON.parse(JSON.stringify(node));
+      }
+    }
+  });
+  links.map(link => {
+    if (!flatNodes[link.source]) {
+      console.error(`Node ${link.source} is not exist!`);
+    } else if (!flatNodes[link.target]) {
+      console.error(`Node ${link.target} is not exist!`);
+    } else {
+      linesData.push({
+        coords: [
+          [
+            flatNodes[link.source].location.lng,
+            flatNodes[link.source].location.lat
+          ],
+          [
+            flatNodes[link.target].location.lng,
+            flatNodes[link.target].location.lat
+          ]
+        ],
+        lineStyle:
+          typeof configs.linkStyleProperty === "function"
+            ? configs.linkStyleProperty(link)
+            : configs.linkStyleProperty,
+        link: link
       });
     }
+  });
 
-    let configs = _this.config;
-
-    RenderCache.leafLeyLayers = [];
-    RenderCache.leafLeyLayers.push(
-      L.tileLayer(
-        "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
-        {
-          attribution:
-            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: configs.scaleExtent[1],
-          id: "mapbox.streets",
-          accessToken:
-            "pk.eyJ1Ijoia3V0dWd1IiwiYSI6ImNqdHpnb2hqMjM0OG40OHBjbmN3azV1b2UifQ.PBk9TefuYkZlK8SweLAebA"
-        }
-      ).addTo(map)
-    );
-
-    for (let node_id in flatNodes) {
-      if (flatNodes[node_id].location) {
-        let { location, ...res } = flatNodes[node_id];
-        nodeElements.push(
-          L.circleMarker(
-            [location.lng, location.lat],
-            Object.assign(
-              {
-                radius:
-                  typeof configs.nodeSize === "function"
-                    ? configs.nodeSize(flatNodes[node_id])
-                    : configs.nodeSize
-              },
-              typeof configs.nodeStyleProperty === "function"
-                ? configs.nodeStyleProperty(flatNodes[node_id])
-                : configs.nodeStyleProperty,
-              { params: JSON.parse(JSON.stringify(res)) }
-            )
-          ).bindTooltip(_this.utils.nodeInfo(res))
-        );
-      }
-    }
-    for (let link of links) {
-      if (flatNodes[link.source] && flatNodes[link.target]) {
-        linkElements.push(
-          L.polyline(
-            [
-              [
-                flatNodes[link.source].location.lng,
-                flatNodes[link.source].location.lat
-              ],
-              [
-                flatNodes[link.target].location.lng,
-                flatNodes[link.target].location.lat
-              ]
-            ],
-            Object.assign(
-              typeof configs.linkStyleProperty === "function"
-                ? configs.linkStyleProperty(link)
-                : configs.linkStyleProperty,
-              { params: JSON.parse(JSON.stringify(link)) }
-            )
-          ).bindTooltip(_this.utils.linkInfo(link))
-        );
-      }
-    }
-    drawElements.push(
-      L.featureGroup(nodeElements).on("click", function(e) {
-        map.setView([e.latlng.lat, e.latlng.lng], map.getMaxZoom());
-        configs.onClickNode.call(_this, e.layer.options.params);
+  let series = [
+    ...configs.mapLineConfig.map(lineConfig =>
+      Object.assign(lineConfig, {
+        type: "lines",
+        coordinateSystem: "leaflet",
+        data: linesData
       })
-    );
-    drawElements.push(
-      L.featureGroup(linkElements).on("click", function(e) {
-        map.setView([e.latlng.lat, e.latlng.lng]);
-        configs.onClickLink.call(_this, e.layer.options.params);
-      })
-    );
-    L.featureGroup(drawElements).addTo(map);
-  }
-  viewInputImage(map, _this);
+    ),
+    Object.assign(configs.mapNodeConfig, {
+      type: "effectScatter",
+      coordinateSystem: "leaflet",
+      data: nodesData
+    })
+  ];
 
-  return map;
+  const graph = echarts.init(mapContainer, null, {
+    renderer: configs.svgRender ? "svg" : "canvas"
+  });
+  _this.echarts = graphSetOption(
+    {
+      leaflet: {
+        tiles: [
+          {
+            urlTemplate: configs.mapTileConfig[0],
+            options: configs.mapTileConfig[1]
+          }
+        ],
+        center: configs.mapCenter.reverse(),
+        zoom: configs.mapZoom,
+        roam: configs.mapRoam
+      },
+      toolbox: {
+        show: false
+      },
+      series
+    },
+    graph,
+    _this
+  );
+  _this.leaflet = graph._api.getCoordinateSystems()[0].getLeaflet();
+
+  configs.onLoad.call(_this);
 }
 
 /**
@@ -325,92 +261,72 @@ function mapRender(mapContainer, JSONData, _this) {
  *
  * Add Input to upload indoormap image.
  *
- * @param  {object}   netjsonmap
- * @param  {object}   _this           NetJSONGraph object
+ * @param  {string}   img             Indoor img src
+ * @param  {object}   _this           NetJSONMap object
  *
  * @return {object}   input DOM
  */
 
-function viewInputImage(netjsonmap, _this) {
-  let imgInput = document.getElementById("njg-indoorImgInput");
-  if (RenderCache.viewIndoormap) {
-    presentIndoormap(imgInput.files[0]);
-  } else if (!imgInput) {
-    imgInput = document.createElement("input");
-    imgInput.setAttribute("type", "file");
-    imgInput.setAttribute("accept", "image/*");
-    imgInput.setAttribute("id", "njg-indoorImgInput");
-    _this.el.appendChild(imgInput);
-  }
-  imgInput.onchange = e => {
-    presentIndoormap(e.target.files[0]);
-  };
+function presentIndoormap(img, _this) {
+  let netjsonmap = _this.leaflet,
+    tempImage = new Image();
 
-  return imgInput;
-
-  function presentIndoormap(img) {
-    let readImg = new FileReader(),
-      tempImage = new Image();
-    readImg.readAsDataURL(img);
-    readImg.onload = e => {
-      tempImage.src = e.target.result;
-      tempImage.onload = () => {
-        let southWest, northEast, bounds;
-        if (
-          tempImage.width / tempImage.height >
-          window.innerWidth / window.innerHeight
-        ) {
-          (southWest = netjsonmap.layerPointToLatLng(
-            L.point(
-              0,
-              window.innerHeight -
-                (window.innerHeight -
-                  (window.innerWidth * tempImage.height) / tempImage.width) /
-                  2 +
-                60
-            )
-          )),
-            (northEast = netjsonmap.layerPointToLatLng(
-              L.point(
-                window.innerWidth,
-                (window.innerHeight -
-                  (window.innerWidth * tempImage.height) / tempImage.width) /
-                  2 +
-                  60
-              )
-            ));
-          bounds = new L.LatLngBounds(southWest, northEast);
-        } else {
-          (southWest = netjsonmap.layerPointToLatLng(
-            L.point(
+  tempImage.src = img;
+  tempImage.onload = () => {
+    let southWest, northEast, bounds;
+    if (
+      tempImage.width / tempImage.height >
+      window.innerWidth / window.innerHeight
+    ) {
+      (southWest = netjsonmap.layerPointToLatLng(
+        L.point(
+          0,
+          window.innerHeight -
+            (window.innerHeight -
+              (window.innerWidth * tempImage.height) / tempImage.width) /
+              2 +
+            60
+        )
+      )),
+        (northEast = netjsonmap.layerPointToLatLng(
+          L.point(
+            window.innerWidth,
+            (window.innerHeight -
+              (window.innerWidth * tempImage.height) / tempImage.width) /
+              2 +
+              60
+          )
+        ));
+      bounds = new L.LatLngBounds(southWest, northEast);
+    } else {
+      (southWest = netjsonmap.layerPointToLatLng(
+        L.point(
+          (window.innerWidth -
+            (window.innerHeight * tempImage.width) / tempImage.height) /
+            2,
+          window.innerHeight + 60
+        )
+      )),
+        (northEast = netjsonmap.layerPointToLatLng(
+          L.point(
+            window.innerWidth -
               (window.innerWidth -
                 (window.innerHeight * tempImage.width) / tempImage.height) /
                 2,
-              window.innerHeight + 60
-            )
-          )),
-            (northEast = netjsonmap.layerPointToLatLng(
-              L.point(
-                window.innerWidth -
-                  (window.innerWidth -
-                    (window.innerHeight * tempImage.width) / tempImage.height) /
-                    2,
-                60
-              )
-            ));
-          bounds = new L.LatLngBounds(southWest, northEast);
-        }
-        for (let layer of RenderCache.leafLeyLayers) {
-          netjsonmap.removeLayer(layer);
-        }
-        RenderCache.leafLeyLayers.push(
-          L.imageOverlay(tempImage.src, bounds).addTo(netjsonmap)
-        );
-        RenderCache.viewIndoormap = true;
-      };
-    };
-  }
+            60
+          )
+        ));
+      bounds = new L.LatLngBounds(southWest, northEast);
+    }
+    netjsonmap.eachLayer(layer => {
+      if (layer._url) {
+        netjsonmap.removeLayer(layer);
+      }
+    });
+    L.imageOverlay(tempImage.src, bounds).addTo(netjsonmap);
+  };
 }
 
 window.graphRender = graphRender;
 window.mapRender = mapRender;
+window.presentIndoormap = presentIndoormap;
