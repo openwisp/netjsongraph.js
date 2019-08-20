d10dba5df03f8d069bfdcb86d2504c33262b6fe1
KuTuGu <256317135@qq.com>
2019-08-20 09:56:07 +0800

    fix: fix test

--- test/netjsongraph.render.test.js
+++ test/netjsongraph.render.test.js
@@ -15,8 +15,9 @@ window.fetch = jest.fn(url => url === JSONFILE ?
   Promise.reject("Fetch json file wrong!")
 );
 
-const graph = new NetJSONGraph(JSONFILE);
+const graph = new NetJSONGraph([JSONFILE, JSONFILE]);
 graph.utils = Object.assign(new NetJSONGraphUpdate(), graph.utils);
+graph.event = graph.utils.createEvent();
 graph.setConfig({
   el: document.getElementsByTagName("body")[0],
   nodeSize: 5,
@@ -24,6 +25,9 @@ graph.setConfig({
     return this.config;
   },
   onRender: function() {
+    return this.config;
+  },
+  onUpdate: function() {
     return this.config;
   },
   onLoad: function() {
@@ -34,6 +38,10 @@ graph.setConfig({
 
 describe("Test netjsongraph render", () => {
   test("netjsongraph.js render function", () => {
+    graph.setConfig({
+      dealDataByWorker: "./error.js",
+    })
+
     expect(graph.render());
 
     // re render
@@ -43,39 +51,60 @@ describe("Test netjsongraph render", () => {
 
 describe("Modify netjsongraph configs", () => {
   test("NetJSONGraph support dynamic modification of config parameters", () => {
-    graph.setConfig({nodeSize: 1});
+    graph.setConfig({
+      nodeSize: 1
+    });
     graph.setConfig();
     expect(graph.config.nodeSize).toBe(1);
   });
 })
 
 describe("Test netjsongraph JSONDataUpdate", () => {
-  graph.render();
+  // graph.render();
 
   test("Callback function executed when data update.Update Information and view.", () => {
-    expect(graph.utils.JSONDataUpdate.call(graph, {
+    graph.utils.JSONDataUpdate.call(graph, {
       metadata: {},
       date: "2019-04-03T09:06:54.000Z",
       nodes: [{id: "1"}],
       links: [{id: "2"}],
-    }))
-  })
-  test("Update metadata test.", () => {
-    expect(graph.utils.JSONDataUpdate.call(graph, {
-      metadata: {},
-      nodes: [],
-      links: [],
-    }))
-  })
-  test("Update metadata test.", () => {
-    graph.setConfig({
-      metadata: false
     })
-    expect(graph.utils.JSONDataUpdate.call(graph, {
+  })
+  test("Update metadata test.", () => {
+    graph.utils.JSONDataUpdate.call(graph, {
       metadata: {},
       nodes: [],
       links: [],
-    }))
+    }, true, false)
+  })
+  test("Update metadata false test.", () => {
+    graph.setConfig({
+      metadata: false,
+    })
+    graph.render();
+    graph.utils.JSONDataUpdate.call(graph, {
+      metadata: {},
+      nodes: [],
+      links: [],
+    }, true, false);
+  })
+  test("Deal with webWorker.", () => {
+    graph.utils.JSONDataUpdate.call(graph, {
+      nodes: [],
+      links: [],
+    });
+  })
+  test("Append data.", () => {
+    graph.setConfig({
+      dealDataByWorker: "",
+    })
+    graph.utils.JSONDataUpdate.call(graph, {
+      nodes: [],
+      links: [],
+    }, false);
+  })
+  test("JSON update event emit.", () => {
+    graph.event.emit("renderArray");
   })
 })
 
@@ -104,8 +133,17 @@ describe("Test netjsongraph searchElements", () => {
   test("Add search function for new elements.", () => {
     let searchFunc = graph.utils.searchElements("test");
 
+    window.history.pushState({ searchValue: "" }, "");
     searchFunc("false").catch(e => {
       expect(e).toMatch("Fetch json file wrong!")
     })
+  })
+
+  test("Search same key.", () => {
+    const searchFunc = graph.utils.searchElements("test"),
+          key = "false";
+
+    window.history.pushState({ searchValue: key }, "");
+    searchFunc(key);
   })
 })
