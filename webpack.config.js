1ea5f9692c012a8efeed4b2603cb64a676d06aa5
Vaishnav Nair <nairvaishnav007@icloud.com>
2022-10-24 18:53:59 +0530

    [change] Allow hot module reloading #151
    closes #151

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,27 @@
 const path = require("path");
+const fs = require("fs");
 const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const examples = fs.readdirSync(
+  path.resolve(__dirname, "public/example_templates"),
+);
+const templates = examples.map((example) => {
+  return new HtmlWebpackPlugin({
+    filename: `examples/${example}`,
+    inject: "head",
+    scriptLoading: "blocking",
+    template: path.resolve(__dirname, `public/example_templates/${example}`),
+  });
+});
 
 module.exports = (env, argv) => ({
   entry: "./src/js/netjsongraph.js",
   output: {
-    path: path.resolve(__dirname, "./dist"),
+    path: path.resolve(__dirname, "dist"),
     filename: "netjsongraph.min.js",
   },
-  devtool:
-    argv.mode === "development" ? "cheap-module-source-map" : "source-map",
+  devtool: argv.mode === "development" ? "eval-source-map" : "source-map",
   optimization: {
     minimize: true,
     minimizer: [
@@ -26,10 +39,13 @@ module.exports = (env, argv) => ({
     ],
   },
   devServer: {
-    static: "./",
+    static: {
+      directory: path.join(__dirname, "/"),
+    },
     historyApiFallback: true,
     open: ["./index.html"],
   },
+  plugins: [...templates],
   performance: {
     hints: false,
   },
