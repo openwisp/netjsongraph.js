9eefdc15a82383e93b60e2a9dd6c1bc566755490
Vaishnav Nair <nairvaishnav007@icloud.com>
2022-11-04 21:29:20 +0530

    [ci] Deploy to gh-pages automatically #172
    closes #172

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,20 @@ const path = require("path");
 const fs = require("fs");
 const TerserPlugin = require("terser-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 const examples = fs.readdirSync(
   path.resolve(__dirname, "public/example_templates"),
 );
-const templates = examples.map((example) => {
-  return new HtmlWebpackPlugin({
-    filename: `examples/${example}`,
-    inject: "head",
-    scriptLoading: "blocking",
-    template: path.resolve(__dirname, `public/example_templates/${example}`),
-  });
-});
+const templates = examples.map(
+  (example) =>
+    new HtmlWebpackPlugin({
+      filename: `examples/${example}`,
+      inject: "head",
+      scriptLoading: "blocking",
+      template: path.resolve(__dirname, `public/example_templates/${example}`),
+    }),
+);
 const minimizer = [
   new TerserPlugin({
     extractComments: false,
@@ -47,7 +49,33 @@ module.exports = (env, argv) => ({
     open: ["./index.html"],
     hot: true,
   },
-  plugins: [...templates],
+  plugins: [
+    ...templates,
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public/assets"),
+          to: path.resolve(__dirname, "dist/assets"),
+        },
+        {
+          from: path.resolve(__dirname, "lib"),
+          to: path.resolve(__dirname, "dist/lib"),
+        },
+        {
+          from: path.resolve(__dirname, "src/css"),
+          to: path.resolve(__dirname, "dist/lib/css"),
+        },
+        {
+          from: path.resolve(__dirname, "src/js/netjsonWorker.js"),
+          to: path.resolve(__dirname, "dist/lib/js/netjsonWorker.js"),
+        },
+        {
+          from: path.resolve(__dirname, "index.html"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
+  ],
   performance: {
     hints: false,
   },
