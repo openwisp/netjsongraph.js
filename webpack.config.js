934f29ff55d0f5f00fab953121ef70f205033ffe
Vaishnav Nair <64798176+totallynotvaishnav@users.noreply.github.com>
2022-11-12 21:32:46 +0530

    [ci] Deploy examples with mapbox tiles to gh-pages #169 #172
    Closes #169
    Closes #172

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,21 @@ const path = require("path");
 const fs = require("fs");
 const TerserPlugin = require("terser-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 const examples = fs.readdirSync(
   path.resolve(__dirname, "public/example_templates"),
 );
-const templates = examples.map((example) => {
-  return new HtmlWebpackPlugin({
-    filename: `examples/${example}`,
-    inject: "head",
-    scriptLoading: "blocking",
-    template: path.resolve(__dirname, `public/example_templates/${example}`),
-  });
-});
+const templates = examples.map(
+  (example) =>
+    new HtmlWebpackPlugin({
+      filename: `examples/${example}`,
+      inject: "head",
+      scriptLoading: "blocking",
+      template: path.resolve(__dirname, `public/example_templates/${example}`),
+    }),
+);
 const minimizer = [
   new TerserPlugin({
     extractComments: false,
@@ -47,7 +50,34 @@ module.exports = (env, argv) => ({
     open: ["./index.html"],
     hot: true,
   },
-  plugins: [...templates],
+  plugins: [
+    ...templates,
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public/assets"),
+          to: path.resolve(__dirname, "dist/assets"),
+        },
+        {
+          from: path.resolve(__dirname, "lib"),
+          to: path.resolve(__dirname, "dist/lib"),
+        },
+        {
+          from: path.resolve(__dirname, "src/css"),
+          to: path.resolve(__dirname, "dist/lib/css"),
+        },
+        {
+          from: path.resolve(__dirname, "src/js/netjsonWorker.js"),
+          to: path.resolve(__dirname, "dist/lib/js/netjsonWorker.js"),
+        },
+        {
+          from: path.resolve(__dirname, "index.html"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
+    new Dotenv({systemvars: true}),
+  ],
   performance: {
     hints: false,
   },
