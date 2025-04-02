4f8614d4c23e59494f804f3569f52075f59985a1
Cestercian <yashafaid@gmail.com>
2025-04-03 04:34:38 +0530

    [fix] Update netjsongraph.config.js

--- src/js/netjsongraph.config.js
+++ src/js/netjsongraph.config.js
@@ -36,7 +36,7 @@ const NetJSONGraphDefaultConfig = {
   loadMoreAtZoomLevel: 9,
   clustering: false,
   clusteringThreshold: 100,
-  disableClusteringAtLevel: 0, // Disables clustering at zoom level 0 when specified in mapOptions
+  disableClusteringAtLevel: 8,
   clusterRadius: 80,
   showMetaOnNarrowScreens: false,
   showLabelsAtZoomLevel: 7,
