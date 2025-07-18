08361a16fbae1e9037d90cc4dac1d9e5fb27cd7c
Cestercian <yashafaid@gmail.com>
2025-07-19 05:05:30 +0530

    [fix] Changed Config to not show Label for test cases

--- src/js/netjsongraph.config.js
+++ src/js/netjsongraph.config.js
@@ -168,7 +168,7 @@ const NetJSONGraphDefaultConfig = {
     nodeConfig: {
       type: "scatter",
       label: {
-        show: false,
+        show: true,
         color: "#000000",
         position: "top",
         formatter: "{b}",
