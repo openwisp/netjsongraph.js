ea99cb55e1c4e9fbc8b5aaef3cf08be2c55cc492
Cestercian <yashafaid@gmail.com>
2025-07-28 01:21:49 +0530

    [fix] QA-checks

--- src/js/netjsongraph.util.js
+++ src/js/netjsongraph.util.js
@@ -576,10 +576,7 @@ class NetJSONGraphUtil {
         if (typeof node.properties[key] === "object" || key.startsWith("_")) {
           return;
         }
-        if (
-          (key === "id" || key === "label") &&
-          identityIsPublic
-        ) {
+        if ((key === "id" || key === "label") && identityIsPublic) {
           return;
         }
         if (key === "location") {
