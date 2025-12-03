5612ae0e9a92c7a3849fd5769df0440342e305dd
Gagan Deep <pandafy.dev@gmail.com>
2025-12-03 19:57:49 +0530

    [qa] Fixed QA errors and updated clustering example

--- src/js/netjsongraph.util.js
+++ src/js/netjsongraph.util.js
@@ -1321,7 +1321,7 @@ class NetJSONGraphUtil {
           ? self.config.disableClusteringAtLevel
           : self.config.bookmarkableActions.zoomLevel ||
             self.config.showLabelsAtZoomLevel;
-      self.leaflet?.setView(center, zoom);
+      self.leaflet && self.leaflet.setView(center, zoom);
     }
     self.config.onClickElement.call(self, source && target ? "link" : "node", node);
   }
