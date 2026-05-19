f981d53070cfc5c40572431a7b6891324c0196fb
Federico Capoano <f.capoano@openwisp.io>
2026-05-19 19:43:58 -0300

    [qa] Trim eslintrc parserOptions to the minimum needed
    ecmaVersion: 2020 is exactly enough to parse optional chaining (?.)
    introduced in this PR; sourceType: "module" is already provided by
    eslint-config-airbnb-base, so drop the redundant line.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   extends: ["airbnb", "prettier"],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    // Override airbnb-config's ecmaVersion: 2018 so optional chaining
+    // (used by gui.js / util.js) parses without errors.
+    ecmaVersion: 2020,
   },
   rules: {
     "no-param-reassign": "off",
